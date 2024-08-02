af1a28eea3ae25c7a407fc6674fbb3aa41cbbe50
Shanmukh459 <saikrrish459@gmail.com>
2024-08-02 12:58:48 -0700

    fetching data using useQuery of react-query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,21 @@
+import { useQuery } from "@tanstack/react-query"
 import genres from "../data/genres"
+import apiClient from "../services/api-client"
+import { FetchResponse } from "./useData"
 export interface Genre {
   id: number
   name: string
   image_background: string
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null })
+// const useGenres = () => ({ data: genres, isLoading: false, error: null })
+
+const useGenres = () => {
+  return useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+  })
+}
 
 export default useGenres
