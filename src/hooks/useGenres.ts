ee38025096546932e33759c130b035b53c73f267
Shanmukh459 <saikrrish459@gmail.com>
2024-08-02 13:11:08 -0700

    fetching platforms using react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -10,14 +10,13 @@ export interface Genre {
 
 // const useGenres = () => ({ data: genres, isLoading: false, error: null })
 
-const useGenres = () => {
-  return useQuery({
+const useGenres = () =>
+  useQuery({
     queryKey: ["genres"],
     queryFn: () =>
       apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000, //24h
     initialData: { count: genres.length, results: genres },
   })
-}
 
 export default useGenres
